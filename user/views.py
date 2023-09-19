3e8dc4b9f618ca045541062d01fcd9c7b9feaf32
arta16h <sevda.hayati2015@gmail.com>
2023-09-20 03:10:38 +0330

    Chore: login view

--- user/views.py
+++ user/views.py
@@ -1,11 +1,12 @@
-import random
+import random, datetime, jwt
 from datetime import timedelta
 
-from django.shortcuts import render
 from django.utils import timezone
+from django.contrib.auth.models import update_last_login
 
 from rest_framework import status
 from rest_framework.views import Response, APIView
+from rest_framework.exceptions import AuthenticationFailed
 
 from .models import User
 from .serializer import UserSerializer,UserVerifySerializer
@@ -25,3 +26,32 @@ class RegisterView(APIView):
         user_serializer.is_valid(raise_exception=True)
         user_serializer.save()
         return Response(user_serializer.data, status=status.HTTP_201_CREATED)
+    
+
+class LoginView(APIView):
+
+    def post(self, request):
+        phone = request.data['phone']
+        password = request.data['password']
+        user = User.objects.filter(phone=phone).first()
+
+        if user is None:
+            raise AuthenticationFailed('User not found')
+
+        if not user.check_password(password):
+            raise AuthenticationFailed('Incorrect Password')
+
+        payload = {
+            'id': user.id,
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=60),
+            'iat': datetime.datetime.utcnow(),
+        }
+        token = jwt.encode(payload, 'secret', algorithm='HS256')
+        response = Response()
+        response.set_cookie(key='jwt', value=token, httponly=True)
+        response.data = {
+            'jwt':token
+            }
+
+        update_last_login(None, user)
+        return response
