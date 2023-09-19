9a47522ba93e6659ce4826cedd9a99c2f0d7ab02
arta16h <sevda.hayati2015@gmail.com>
2023-09-20 01:53:36 +0330

    Chore: user model

--- user/models.py
+++ user/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
 # Create your models here.
+
+class User(AbstractBaseUser):
+    email=models.EmailField()
+    phone=models.CharField(max_length=14)
+    username=models.CharField(max_length=50)
+    first_name=models.CharField(max_length=50)
+    last_name=models.CharField(max_length=100)
+    password=models.CharField(max_length=50)
+    address=models.ForeignKey(on_delete=models.CASCADE)
