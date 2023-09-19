c5607942176907f139b4f9b3c3bb72f8191b1c46
arta16h <sevda.hayati2015@gmail.com>
2023-09-20 02:30:36 +0330

    Chore: category model

--- product/models.py
+++ product/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from core.models import BaseModel
 
 # Create your models here.
+
+class Category(BaseModel):
+    name = models.CharField(max_length=50)
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    image = models.ImageField(null=True,blank=True)
+
+    def __str__(self):
+        return self.name
