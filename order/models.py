717ebfd9f9c768eb88bf6a7f89d70c91a8b30392
arta16h <sevda.hayati2015@gmail.com>
2023-09-20 02:48:10 +0330

    Chore: discount model

--- order/models.py
+++ order/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from core.models import BaseModel
 
 # Create your models here.
+
+class Discount(BaseModel):
+    discount = models.IntegerField(max_digits=2, default=0)
+
+    def __str__(self):
+        return self.discount
