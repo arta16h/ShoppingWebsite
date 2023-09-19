47d24bf910df6800d7bd0d6eb0f071838623ebc9
arta16h <sevda.hayati2015@gmail.com>
2023-09-20 03:02:47 +0330

    Chore: 2fa

--- user/views.py
+++ user/views.py
@@ -1,3 +1,12 @@
+import random
+from datetime import timedelta
 from django.shortcuts import render
+from django.utils import timezone
 
 # Create your views here.
+
+def generate_2fa(request): 
+    request.session["2FA"] = random.randint(1000, 9999)
+    request.session["2fa_expire"] = (timezone.now() + timedelta(minutes=1)).strftime("%d/%m/%Y, %H:%M:%S")
+    print(f"generated:{request.session['2FA']}  exp:{request.session['2fa_expire']}")
+    return request  
