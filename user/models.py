ea5c099de383cefe321882b804027ead7385d584
arta16h <sevda.hayati2015@gmail.com>
2023-09-20 02:13:53 +0330

    Chore: phone number field

--- user/models.py
+++ user/models.py
@@ -8,18 +8,21 @@ from django.contrib.auth.models import AbstractBaseUser
 EMAIL_REGEX_PATTERN = r"([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+"
 PHONE_REGEX_PATTERN = r"(0|\+98)?([ ]|-|[()]){0,2}9[1|2|3|4]([ ]|-|[()]){0,2}(?:[0-9]([ ]|-|[()]){0,2}){8}"
 
+
 def email_validator(email) :
     if not (matched := re.fullmatch(EMAIL_REGEX_PATTERN, email.strip())):
         raise ValidationError("Invalid Email!")
     return matched
+
 
 def phone_validator(phone):
     if not (matched := re.fullmatch(PHONE_REGEX_PATTERN, phone.strip())):
         raise ValidationError("Invalid phone number!")
     return matched
 
+
 class EmailField(models.CharField):
-    def get_value(self, value):
+    def get_email_value(self, value):
         if value is None:
             return value
         try:
@@ -28,6 +31,19 @@ class EmailField(models.CharField):
             raise
 
         return regex
+    
+
+class PhoneNumberField(models.CharField):
+    def get_phone_value(self, value):
+        if value is None:
+            return value
+        try:
+            regex = phone_validator(value)
+        except ValidationError:
+            raise
+
+        return regex
+
 
 class User(AbstractBaseUser):
     email = EmailField(validators=[email_validator], unique=True)
