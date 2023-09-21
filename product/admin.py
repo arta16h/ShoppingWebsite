8a8d5c8e9fffccda2c11f9df2c4a151f1ad8aebe
arta16h <sevda.hayati2015@gmail.com>
2023-09-21 15:32:30 +0330

    Chore: product admin

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Product, Category
 
 # Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "price")
+    search_fields = ("name", "category")
+    list_filter = ("category", "is_available")
+    list_per_page = 10
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name")
+    search_fields = ("name")
+    list_per_page = 10
