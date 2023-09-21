62ac760e7a2d0971a2761add916c8bb925ac8544
arta16h <sevda.hayati2015@gmail.com>
2023-09-21 14:30:53 +0330

    custom user admin

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
 # Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ("phone", "is_staff", "is_active",)
+    list_filter = ("is_staff", "is_active")
+    fieldsets = (
+        (None, {"fields": ("phone_number", "password")}),
+        ("Permissions", {"fields": ("is_staff", "is_active")})
+        )
+    add_fieldsets = (None, {"fields": ("username", "phone", "password", "is_staff", "is_active")})
+    search_fields = ("phone")
+    ordering = ("first_name","last_name")
+
+
+admin.site.register(User, CustomUserAdmin)
