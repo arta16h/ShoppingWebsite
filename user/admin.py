eb2bf7f7409f86ecabaedd06f7f23b24166fe75c
arta16h <sevda.hayati2015@gmail.com>
2023-09-21 15:29:20 +0330

    Chore: address admin

--- user/admin.py
+++ user/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import User, Address
 
 # Register your models here.
 
@@ -15,6 +15,17 @@ class CustomUserAdmin(UserAdmin):
     add_fieldsets = (None, {"fields": ("username", "phone", "password", "is_staff", "is_active")})
     search_fields = ("phone")
     ordering = ("first_name","last_name")
+
+
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    list_display = (
+        "city",
+        "street",
+    )
+    search_fields = ("city","street")
+    list_filter = ("city", "street")
+    list_per_page = 10
 
 
 admin.site.register(User, CustomUserAdmin)
