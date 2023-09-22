618453e19c16ed1baea6937ddef434d89d54eb45
arta16h <sevda.hayati2015@gmail.com>
2023-09-22 15:45:38 +0330

    Refactor: import api view

--- order/views.py
+++ order/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
 from .serializers import AddtoCartSerializer
 
 # Create your views here.
 
-class AddtoCartApiView(ApiView) :
+class AddtoCartApiView(APIView) :
     def get (self, request) :
         serializer = AddtoCartSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True) :
