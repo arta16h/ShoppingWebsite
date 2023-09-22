5907d83ce216be20a085c62cbe15fd8a7c1f395b
arta16h <sevda.hayati2015@gmail.com>
2023-09-22 15:18:28 +0330

    Chore: product list view

--- product/views.py
+++ product/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from .models import Product, Category
+from .serializers import ProductSerializer
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
+
+class ProductListView(APIView):
+    def get(self, request, slug):
+        category = Category.objects.get(slug=slug)
+        products = Product.objects.filter(category=category)
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
