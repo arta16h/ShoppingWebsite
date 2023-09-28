213190467c801a66f6451b0f9fcddaa4fc092e5b
arta16h <sevda.hayati2015@gmail.com>
2023-09-28 08:53:14 +0330

    Refactor

--- user/views.py
+++ user/views.py
@@ -11,7 +11,7 @@ from rest_framework.views import Response, APIView
 from rest_framework.exceptions import AuthenticationFailed
 
 from .models import User
-from .serializer import UserSerializer
+from .serializer import UserSerializer, LoginOTPSerializer, LoginSerializer
 from .utils import generate_access_token, generate_refresh_token
 
 # Create your views here.
@@ -32,45 +32,45 @@ class RegisterView(APIView):
     
 class SendOTPView(APIView):
     def post(self, request):
-        serializer=SerializerLogin(data=request.data, context={'request':request})
+        serializer=LoginSerializer(data=request.data, context={"request":request})
         if serializer.is_valid(raise_exception=True):
-            serializer.create_otp(request, serializer.data['phone'])
-            return Response (data={'message':"200"})
+            serializer.create_otp(request, serializer.data["phone"])
+            return Response (data={"message":"200"})
 
 
 class VerifyOTP(APIView):
     def post(self, request):
-        serliazer=LoginOTPSerializer(data=request.data, context={'request':request})
+        serliazer=LoginOTPSerializer(data=request.data, context={"request":request})
         if serliazer.is_valid(raise_exception=True):
-            user=User.objects.get(phone=request.session.get('phone'))
+            user=User.objects.get(phone=request.session.get("phone"))
             access_token=user.get_access_token()
             refresh_token=user.get_refresh_token()
-            return Response(data={'message':"success", "AT":access_token, "RT":refresh_token})
+            return Response(data={"message":"success", "AT":access_token, "RT":refresh_token})
 
 
 class LoginView(APIView):
 
     def post(self, request):
-        phone = request.data['phone']
-        password = request.data['password']
+        phone = request.data["phone"]
+        password = request.data["password"]
         user = User.objects.filter(phone=phone).first()
 
         if user is None:
-            raise AuthenticationFailed('User not found')
+            raise AuthenticationFailed("User not found")
 
         if not user.check_password(password):
-            raise AuthenticationFailed('Incorrect Password')
+            raise AuthenticationFailed("Incorrect Password")
 
         payload = {
-            'id': user.id,
-            'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=60),
-            'iat': datetime.datetime.utcnow(),
+            "id": user.id,
+            "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=60),
+            "iat": datetime.datetime.utcnow(),
         }
-        token = jwt.encode(payload, 'secret', algorithm='HS256')
+        token = jwt.encode(payload, "secret", algorithm="HS256")
         response = Response()
-        response.set_cookie(key='jwt', value=token, httponly=True)
+        response.set_cookie(key="jwt", value=token, httponly=True)
         response.data = {
-            'jwt':token
+            "jwt":token
             }
 
         update_last_login(None, user)
@@ -90,20 +90,20 @@ class RefreshTokenApiView(APIView):
     permission_classes = [permissions.AllowAny]
 
     def post(self, request):
-        refresh_token = request.COOKIES.get('refreshtoken')
+        refresh_token = request.COOKIES.get("refreshtoken")
         if refresh_token is None:
             raise exceptions.AuthenticationFailed
         try:
-            payload = jwt.decode(refresh_token, settings.REFRESH_TOKEN_SECRET, algorithms=['HS256'])
+            payload = jwt.decode(refresh_token, settings.REFRESH_TOKEN_SECRET, algorithms=["HS256"])
         except jwt.ExpiredSignatureError:
-            raise exceptions.AuthenticationFailed('expired refresh token')
+            raise exceptions.AuthenticationFailed("expired refresh token")
 
-        user = User.objects.filter(id=payload.get('user_id')).first()
+        user = User.objects.filter(id=payload.get("user_id")).first()
         if user is None:
-            raise exceptions.AuthenticationFailed('User not found')
+            raise exceptions.AuthenticationFailed("User not found")
 
         if not user.is_active:
             raise exceptions.AuthenticationFailed("user isn't active")
 
         access_token = generate_access_token(user)
-        return Response({'access_token': access_token})
+        return Response({"access_token": access_token})
