5ffc461c6262fef0af869656559161883b27e978
arta16h <sevda.hayati2015@gmail.com>
2023-09-28 09:35:56 +0330

    Chore: order item serializer

--- order/serializers.py
+++ order/serializers.py
@@ -1,6 +1,21 @@
 from rest_framework import serializers
+from .models import Order, OrderItem
 
 
 class AddtoCartSerializer(serializers.Serializer) :
     product = serializers.CharField()
     quantity = serializers.IntegerField()
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=OrderItem
+        fields=[
+            "orders",
+            "products",
+            "quantity",
+            "unit_price",
+            "discount_amount",
+            ]
+        
+        
