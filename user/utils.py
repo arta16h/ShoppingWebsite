88e301d37950b299d5b92b847b9087d69db6d3fc
arta16h <sevda.hayati2015@gmail.com>
2023-09-28 09:25:50 +0330

    Chore: jwt helper

--- user/utils.py
+++ user/utils.py
@@ -1,5 +1,16 @@
 import datetime, jwt
 from django.conf import settings
+from rest_framework.views import Response
+from datetime import timedelta, datetime
+
+class JwtHelper:
+    @staticmethod
+    def generate_jwt_token(user_id, secret_key, expires_in_minutes):
+        payload = {
+            "user_id": user_id,
+            "exp": datetime.utcnow() + timedelta(minutes=expires_in_minutes)
+        }
+        return jwt.encode(payload, secret_key, algorithm="HS256")
 
 def generate_access_token(user, expiration_time_minutes=60):
     access_token_payload = {
