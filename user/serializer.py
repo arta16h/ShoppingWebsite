ea953db9b505419d13bb52067334c9934f930c59
arta16h <sevda.hayati2015@gmail.com>
2023-09-28 08:48:56 +0330

    Chore: login serializer

--- user/serializer.py
+++ user/serializer.py
@@ -1,12 +1,21 @@
+import random
+from datetime import timedelta
+from django.utils import timezone
 from rest_framework import serializers
 from .models import User, Address
 
 
 class UserSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = User
-        fields = ['username', 'first_name', 'last_name', 'email', 'phone', 'password']
+        fields = [
+            "username",
+            "first_name",
+            "last_name",
+            "email",
+            "phone",
+            "password",
+        ]
 
     def create(self, validate_data):
         password = validate_data.pop("password", None)
@@ -35,10 +44,33 @@ class LoginOTPSerializer(serializers.Serializer):
             raise serializers.ValidationError
 
         return data
+    
+
+class LoginSerializer(serializers.Serializer):
+    phone = serializers.CharField(required=True, allow_null=False)
+
+    def validate(self, data):
+        phone = data.get("phone")
+        if not User.objects.filter(phone=phone).exists():
+            raise serializers.ValidationError
+        return data
+
+    @staticmethod
+    def create_otp(request, phone):
+        request.session["otp"] = random.randint(1000, 9999)
+        request.session["otp_expire"] = (timezone.now() + timedelta(minutes=10)).strftime("%d/%m/%Y, %H:%M:%S")
+        request.session["phone"]=phone
+        print(f"generated:{request.session["otp"]}  until:{request.session["otp_expire"]}")
 
 
 class AddressSerializer(serializers.ModelSerializer):
     class Meta:
         model = Address
-        fields =("province", "city", "street", "detail", "postal_code")
+        fields =[
+            "province", 
+            "city", 
+            "street", 
+            "detail", 
+            "postal_code",
+            ]
 
