c23dd0af1774d451474944f5593e4599a4d46605
arta16h <sevda.hayati2015@gmail.com>
2023-09-28 11:13:41 +0330

    Chore: podcast category url

--- product/urls.py
+++ product/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ProductDetailView, ProductListApiView, SearchProductView
+from .views import ProductDetailView, ProductListApiView, SearchProductView, ProductCategoryView
 
 urlpatterns = [
     path("products/<slug:slug>", ProductListApiView.as_view()),
     path("product/<slug:slug>", ProductDetailView.as_view()),
     path("search", SearchProductView.as_view()),
+    path("podcat/", ProductCategoryView.as_view()),
 ]
