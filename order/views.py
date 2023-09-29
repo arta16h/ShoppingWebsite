6f19925aabf642cf16a220ec27d6bb25079fe1b0
arta16h <sevda.hayati2015@gmail.com>
2023-09-29 15:56:52 +0330

    Chore: delete cart view

--- order/views.py
+++ order/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
 from rest_framework.views import APIView
 from .serializers import AddtoCartSerializer
 
@@ -9,3 +10,13 @@ class AddtoCartApiView(APIView) :
         serializer = AddtoCartSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True) :
             print(serializer.data)
+
+def delete_cart(request, response) -> None:
+    if request.COOKIES.get("cart"):
+        response.delete_cookie("cart")
+
+class DeleteCartView(View):
+    def get(self, request, *args, **kwargs):
+        response = redirect(self.success_redirect_url)
+        delete_cart(request, response)
+        response.delete_cookie("cart")
