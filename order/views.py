80e4cb3841ef53bf0a445b7eaa337af07919eadc
arta16h <sevda.hayati2015@gmail.com>
2023-09-29 15:59:39 +0330

    Chore: orderitem update view

--- order/views.py
+++ order/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+
 from rest_framework.views import APIView
 from .serializers import AddtoCartSerializer
+from .models import OrderItem
 
 # Create your views here.
 
@@ -20,3 +23,18 @@ class DeleteCartView(View):
         response = redirect(self.success_redirect_url)
         delete_cart(request, response)
         response.delete_cookie("cart")
+
+
+class OrderItemUpdateView(LoginRequiredMixin, PermissionRequiredMixin, View):
+    def post(self, request, *args, **kwargs):
+        order_id = kwargs["pk"]
+        order_item_id = request.POST.get("orderitem")
+        quantity = request.POST.get("quantity")
+        order_item = get_object_or_404(OrderItem, pk=int(order_item_id))
+
+        order_item.quantity = int(quantity)
+        order_item.save()
+
+        return redirect("order_details", order_id)
+    
+
